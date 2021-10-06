5e1a013e9a860e2c50d11716e176c80a1088784e
tryspidy <68438592+tryspidy@users.noreply.github.com>
2021-10-06 13:41:32 +0530

    Initial commit

---
+++ main.sql
@@ -0,0 +1,7 @@
+Structured Query Language
+
+A language which enables a user to create, modify and
+essentially interact with a database.
+Two of the most common Database Management Engines are
+MySQL: https://www.mysql.com/
+PostgreSQL: https://www.postgresql.org/
